b0f67b4009b25e97e3b0d43a566f07442582f75c
Hyunggeun <87374446+hyunggeun447@users.noreply.github.com>
2022-08-23 02:07:39 +0900

    Initial commit

---
+++ src/main/java/solo/studyRefeat/StudyRefeatApplication.java
@@ -0,0 +1,13 @@
+package solo.studyRefeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyRefeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyRefeatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/solo/studyRefeat/StudyRefeatApplicationTests.java
@@ -0,0 +1,13 @@
+package solo.studyRefeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyRefeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
