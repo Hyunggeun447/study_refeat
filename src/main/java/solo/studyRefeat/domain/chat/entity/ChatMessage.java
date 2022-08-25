795e6728bdb77be9769a0aad353a8ca5632c1e68
Hyunggeun447 <bopi447@naver.com>
2022-08-25 17:17:43 +0900

    feat: chatMessage 유저 연결 및 메세지 필드 추가

--- src/main/java/solo/studyRefeat/domain/chat/entity/ChatMessage.java
+++ src/main/java/solo/studyRefeat/domain/chat/entity/ChatMessage.java
@@ -7,11 +7,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
+import org.hibernate.validator.constraints.Length;
+import solo.studyRefeat.domain.user.entity.User;
 
 @Entity
 @Table(name = "chat")
@@ -19,11 +22,18 @@ import org.hibernate.annotations.Where;
 @SQLDelete(sql = "UPDATE chat SET is_deleted = true WHERE id = ?")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Chat {
+public class ChatMessage {
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
+
+  @NotBlank
+  @Length(min = 1, max = 2000)
+  private String message;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  private User sender;
 
   @ManyToOne(fetch = FetchType.LAZY)
   private ChatRoom chatRoom;
