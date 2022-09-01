26f623daab1094e43352e58d81c41a0136116dea
Hyunggeun447 <bopi447@naver.com>
2022-09-01 21:11:55 +0900

    test: 영속성 테스트 확인

---
+++ src/test/java/solo/studyRefeat/domain/chat/service/ChatRoomServiceTest.java
@@ -0,0 +1,77 @@
+package solo.studyRefeat.domain.chat.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import solo.studyRefeat.domain.chat.dto.CreateChatRoomRequest;
+import solo.studyRefeat.domain.chat.dto.DeleteChatUserRequest;
+import solo.studyRefeat.domain.chat.entity.ChatRoom;
+import solo.studyRefeat.domain.chat.entity.ChatUser;
+import solo.studyRefeat.domain.chat.repository.ChatRoomRepository;
+import solo.studyRefeat.domain.user.entity.User;
+import solo.studyRefeat.domain.user.repository.UserRepository;
+
+@SpringBootTest
+@Transactional
+class ChatRoomServiceTest {
+
+  @Autowired
+  ChatRoomService chatRoomService;
+
+  @Autowired
+  UserRepository userRepository;
+
+  @Autowired
+  ChatRoomRepository chatRoomRepository;
+
+  @BeforeEach
+  void setup() {
+    User user = userRepository.save(new User("nick"));
+    userId = user.getId();
+    List<Long> userIds = new ArrayList<>();
+    userIds.add(userId);
+    CreateChatRoomRequest request = CreateChatRoomRequest.builder()
+        .roomName("roomName")
+        .userIds(userIds)
+        .build();
+    chatRoomId = chatRoomService.createChatRoom(request);
+
+    ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId).orElseThrow(RuntimeException::new);
+
+    ChatUser chatUser = chatRoom.getChatUsers().get(0);
+    String nickname = chatUser.getUser().getNickname();
+    System.out.println("nickname = " + nickname);
+  }
+
+  Long userId;
+  Long chatRoomId;
+
+  @Test
+  @DisplayName("")
+  public void Test() throws Exception {
+
+//    System.out.println(chatRoomId);
+//    System.out.println(userId);
+    DeleteChatUserRequest request = DeleteChatUserRequest.builder()
+        .chatRoomId(chatRoomId)
+        .userId(userId)
+        .build();
+    chatRoomService.deleteChatUser(request);
+
+    ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId).orElseThrow(RuntimeException::new);
+
+    Assertions.assertThat(chatRoom.getChatUsers().size()).isEqualTo(0);
+//    String nickname = chatUser.getUser().getNickname();
+//    System.out.println("nickname = " + nickname);
+  }
+
+
+}
