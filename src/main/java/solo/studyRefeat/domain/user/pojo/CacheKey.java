1abba6e219e03ce9808f5f38f53c60f80de45061
Hyunggeun447 <bopi447@naver.com>
2022-09-02 18:00:11 +0900

    feat: pojo

---
+++ src/main/java/solo/studyRefeat/domain/user/pojo/CacheKey.java
@@ -0,0 +1,8 @@
+package solo.studyRefeat.domain.user.pojo;
+
+public class CacheKey {
+
+  public static final String USER = "user";
+  public static final int DEFAULT_EXPIRE_SEC = 60;
+
+}
---
+++ src/main/java/solo/studyRefeat/domain/user/pojo/RefreshToken.java
@@ -0,0 +1,37 @@
+package solo.studyRefeat.domain.user.pojo;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+@RedisHash("refreshToken")
+@Getter
+@Builder
+public class RefreshToken {
+
+  @Id
+  private String email;
+
+  private String refreshToken;
+
+  @TimeToLive
+  private Long expiration;
+
+  public static RefreshToken createRefreshToken(String email, String refreshToken, Long remainingMilliSeconds) {
+    return RefreshToken.builder()
+        .email(email)
+        .refreshToken(refreshToken)
+        .expiration(remainingMilliSeconds)
+        .build();
+  }
+
+  //TODO: exception 추가 NotMatchTokenException
+  public void checkToken(String token) {
+    if(!token.equals(this.refreshToken)) {
+      throw new IllegalArgumentException();
+    }
+  }
+
+}
