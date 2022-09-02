26e230f6ee95d9fd2d09e837075bb7d6f15779d3
Hyunggeun447 <bopi447@naver.com>
2022-09-02 17:53:22 +0900

    feat: find 메서드 추가

--- src/main/java/solo/studyRefeat/domain/user/repository/UserRepository.java
+++ src/main/java/solo/studyRefeat/domain/user/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package solo.studyRefeat.domain.user.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import solo.studyRefeat.domain.user.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+  Optional<User> findByEmail(String email);
+  Optional<User> findByNickname(String nickname);
 
 }
