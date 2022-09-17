756b31590a2a42cc68f29452df3bbc97096a755d
Hyunggeun447 <bopi447@naver.com>
2022-09-17 18:50:52 +0900

    feat: chatMessageRepo queryDsl 연결

--- src/main/java/solo/studyRefeat/domain/chat/repository/ChatMessageRepository.java
+++ src/main/java/solo/studyRefeat/domain/chat/repository/ChatMessageRepository.java
@@ -3,6 +3,8 @@ package solo.studyRefeat.domain.chat.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import solo.studyRefeat.domain.chat.entity.ChatMessage;
 
-public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long>,
+    ChatMessageQueryDslRepository {
+
 
 }
