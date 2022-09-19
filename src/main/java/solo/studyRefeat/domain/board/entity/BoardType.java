0506656a576e85d3158b1d2997a2e3d86c0e5b81
Hyunggeun447 <bopi447@naver.com>
2022-09-19 22:25:59 +0900

    feat: 게시판 타입은 이넘으로

---
+++ src/main/java/solo/studyRefeat/domain/board/entity/BoardType.java
@@ -0,0 +1,15 @@
+package solo.studyRefeat.domain.board.entity;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum BoardType {
+
+  FREE("자유게시판"),
+
+  HUMOR("유머게시판")
+  ;
+
+  private final String category;
+
+}
