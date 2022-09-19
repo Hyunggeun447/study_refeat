fdba613f29d10e8418a0bb0a6c3a71e6612eb9bd
Hyunggeun447 <bopi447@naver.com>
2022-09-19 22:25:48 +0900

    feat: bourd entity 기본 추가

---
+++ src/main/java/solo/studyRefeat/domain/board/entity/Board.java
@@ -0,0 +1,61 @@
+package solo.studyRefeat.domain.board.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import solo.studyRefeat.domain.user.entity.User;
+
+@Entity
+@Table(name = "board")
+@Where(clause = "is_deleted = false")
+@SQLDelete(sql = "UPDATE board SET is_deleted = true WHERE id = ?")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Board {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @Column(name = "title")
+  private String title;
+
+  @Column(name = "content")
+  private String content;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  private User user;
+
+  @Enumerated
+  @Column(name = "boardType")
+  private BoardType boardType;
+
+  @Builder.Default
+  @ElementCollection(fetch = FetchType.EAGER)
+  private List<String> imageUrls = new ArrayList<>();
+
+  @Builder.Default
+  @Column(name = "is_deleted")
+  private Boolean isDeleted = Boolean.FALSE;
+
+}
