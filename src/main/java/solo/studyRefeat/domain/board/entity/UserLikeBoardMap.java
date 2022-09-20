51ecdd10c93dc28407607cd308a8af69b3dc9b4c
Hyunggeun447 <bopi447@naver.com>
2022-09-20 23:03:23 +0900

    feat: UserLikeBoardMap 생성자 연관관계 매서드로 변경

--- src/main/java/solo/studyRefeat/domain/board/entity/UserLikeBoardMap.java
+++ src/main/java/solo/studyRefeat/domain/board/entity/UserLikeBoardMap.java
@@ -39,8 +39,8 @@ public class UserLikeBoardMap {
   private User user;
 
   public UserLikeBoardMap(Board board, User user) {
-    this.board = board;
-    this.user = user;
+    addBoard(board);
+    addUser(user);
   }
 
   public void addBoard(Board board) {
