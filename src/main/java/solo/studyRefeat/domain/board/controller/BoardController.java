1feda7c7d121e02655f52143e4d0d92005149d31
Hyunggeun447 <bopi447@naver.com>
2022-09-23 23:10:38 +0900

    feat: 컨트롤러 생성 및 생성 api

---
+++ src/main/java/solo/studyRefeat/domain/board/controller/BoardController.java
@@ -0,0 +1,40 @@
+package solo.studyRefeat.domain.board.controller;
+
+import java.util.List;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import solo.studyRefeat.domain.board.dto.CreateBoardRequest;
+import solo.studyRefeat.domain.board.service.BoardService;
+import solo.studyRefeat.domain.user.aop.annotation.CurrentUser;
+import solo.studyRefeat.domain.user.entity.User;
+import solo.studyRefeat.domain.user.pojo.CustomUserDetails;
+import solo.studyRefeat.domain.user.service.UserService;
+
+@RestController
+@RequestMapping("/v1/board")
+public class BoardController {
+
+  private final BoardService boardService;
+  private final UserService userService;
+
+  public BoardController(BoardService boardService,
+      UserService userService) {
+    this.boardService = boardService;
+    this.userService = userService;
+  }
+
+  @PostMapping()
+  public Long createBoard(
+      @RequestPart("request") CreateBoardRequest request,
+      @RequestPart("images") List<MultipartFile> images,
+      @CurrentUser CustomUserDetails userDetails) {
+    User user = userService.checkUser(userDetails);
+    Long boardId = boardService.createBoard(request, images, user);
+    return boardId;
+  }
+}
--- src/main/java/solo/studyRefeat/domain/user/service/UserService.java
+++ src/main/java/solo/studyRefeat/domain/user/service/UserService.java
@@ -19,4 +19,5 @@ public class UserService {
         .orElseThrow(RuntimeException::new);
   }
 
+
 }
