ff9ae65c1b7045fd34725addef1b29e75bc6ba8a
Hyunggeun447 <bopi447@naver.com>
2022-09-24 09:37:51 +0900

    refactor: JpaQueryFactory 의존성 주입

--- src/main/java/solo/studyRefeat/domain/board/repository/BoardSearchRepositoryImpl.java
+++ src/main/java/solo/studyRefeat/domain/board/repository/BoardSearchRepositoryImpl.java
@@ -1,5 +1,15 @@
 package solo.studyRefeat.domain.board.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
+
 public class BoardSearchRepositoryImpl implements BoardSearchRepository{
+
+  private final JPAQueryFactory jpaQueryFactory;
+
+  public BoardSearchRepositoryImpl(EntityManager em) {
+    this.jpaQueryFactory = new JPAQueryFactory(em);
+  }
+
 
 }
