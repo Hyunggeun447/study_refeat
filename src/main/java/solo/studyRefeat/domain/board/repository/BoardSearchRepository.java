461370b4edafebace2f44a6f7803065e5bbcc35f
Hyunggeun447 <bopi447@naver.com>
2022-09-28 23:46:06 +0900

    feat:

--- src/main/java/solo/studyRefeat/domain/board/repository/BoardSearchRepository.java
+++ src/main/java/solo/studyRefeat/domain/board/repository/BoardSearchRepository.java
@@ -1,5 +1,13 @@
 package solo.studyRefeat.domain.board.repository;
 
+import java.util.Optional;
+import org.springframework.data.domain.Slice;
+import solo.studyRefeat.domain.board.entity.Board;
+import solo.studyRefeat.domain.board.value.BoardType;
+import solo.studyRefeat.domain.board.value.SearchType;
+
 public interface BoardSearchRepository {
+
+  Slice<Board> findByKeyword(Optional<String> keyword, BoardType boardType, SearchType searchType);
 
 }
