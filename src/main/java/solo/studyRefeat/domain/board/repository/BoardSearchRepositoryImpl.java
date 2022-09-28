a3e6796eaa499c376e9397f0fe0a5cc24c270919
Hyunggeun447 <bopi447@naver.com>
2022-09-28 23:46:24 +0900

    feat:

--- src/main/java/solo/studyRefeat/domain/board/repository/BoardSearchRepositoryImpl.java
+++ src/main/java/solo/studyRefeat/domain/board/repository/BoardSearchRepositoryImpl.java
@@ -1,7 +1,12 @@
 package solo.studyRefeat.domain.board.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.Optional;
 import javax.persistence.EntityManager;
+import org.springframework.data.domain.Slice;
+import solo.studyRefeat.domain.board.entity.Board;
+import solo.studyRefeat.domain.board.value.BoardType;
+import solo.studyRefeat.domain.board.value.SearchType;
 
 public class BoardSearchRepositoryImpl implements BoardSearchRepository{
 
@@ -12,4 +17,12 @@ public class BoardSearchRepositoryImpl implements BoardSearchRepository{
   }
 
 
+  @Override
+  public Slice<Board> findByKeyword(Optional<String> keyword, BoardType boardType,
+      SearchType searchType) {
+
+//    jpaQueryFactory.selectFrom(board)
+//        .where(searchType.toBooleanBuilder(keyword))
+    return null;
+  }
 }
